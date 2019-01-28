3f83541e5501753da42ad99bed16c035b5ff0102
Maxcell Wilson <maxcell.wilson@gmail.com>
2019-01-28 13:43:00 -0500

    Updates to include better comments

--- 09-sinatra-intro/sinatra_app/app/controllers/application_controller.rb
+++ 09-sinatra-intro/sinatra_app/app/controllers/application_controller.rb
@@ -12,25 +12,27 @@ class ApplicationController < Sinatra::Base
   set :views, File.join(root, "views")
 
 
-  # I want to see all of the films in the database
+  # Remember that when building out a route, we want
+  # to tell people the request method and the URL
+
+  # Here I want to see all of the films in the database
   get '/films' do
+    # We're going to tell the controller what to do when it sees this request
     # Think about this as if I were in the rake console
     @films = Film.all
 
-    # What view to show
+    # Then we need to tell it what view to show
+    # erb is looking for a file path and so it is expecting a symbol
     erb :films # '/views/films.erb'
   end
 
+  # Here we are going to get a *specifc* item
+  # We need to have the variable input so that way we can show
+  # different things depending on the route
   get '/films/:id' do
+    # We can also test what data is coming by using binding.pry still
     # binding.pry
     @film = Film.find(params[:id])
-
     erb :show
   end
-
-  # get '/dogs' do
-  #   # Get all the Dogs (Who let the dogs out)
-  # end
-
-
 end
